40a3333ea11a4911e24245a02db63fbdb6c01e9d
Bailey Capuano <bcapuano@asu.edu>
2022-12-31 20:31:06 -0500

    Initial MayhemHeroes Integration

---
+++ mayhem/fuzz_reader.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+import random
+
+import atheris
+import sys
+import io
+import fuzz_helpers
+import random
+from contextlib import contextmanager
+
+with atheris.instrument_imports(include=['pymzml']):
+    import pymzml
+
+# Exceptions
+from xml.etree.ElementTree import ParseError
+from gzip import BadGzipFile
+from zlib import error
+
+
+@contextmanager
+def nostdout():
+    save_stdout = sys.stdout
+    save_stderr = sys.stderr
+    sys.stdout = io.StringIO()
+    sys.stderr = io.StringIO()
+    yield
+    sys.stdout = save_stdout
+    sys.stderr = save_stderr
+
+
+def TestOneInput(data):
+    fdp = fuzz_helpers.EnhancedFuzzedDataProvider(data)
+    try:
+        with fdp.ConsumeTemporaryFile('.chrm.mzML.gz', all_data=True) as f, nostdout():
+            pymzml.run.Reader(f)
+    except (ParseError, BadGzipFile, UnicodeDecodeError, error, EOFError):
+        return -1
+    except Exception as e:
+        if 'not a gzip' in str(e):
+            return -1
+        print(type(e), e)
+        raise e
+    except LookupError:
+        if random.random() > 0.99:
+            raise
+        return 0
+
+
+def main():
+    atheris.Setup(sys.argv, TestOneInput)
+    atheris.Fuzz()
+
+
+if __name__ == "__main__":
+    main()
